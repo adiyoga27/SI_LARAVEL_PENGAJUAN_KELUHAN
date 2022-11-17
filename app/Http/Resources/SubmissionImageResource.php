66ae0f2a08f71eec8778a41ce92d670008f78ba9
Wahyu <devcodebali@gmail.com>
2022-11-17 08:33:19 +0800

    url storage

--- app/Http/Resources/SubmissionImageResource.php
+++ app/Http/Resources/SubmissionImageResource.php
@@ -16,7 +16,7 @@ class SubmissionImageResource extends JsonResource
     {
         return [
             'task_id' => $this->task_id,
-            'url' => $this->url,
+            'url' => "https://ppju.codingaja.com/storage/" . $this->url,
         ];
     }
 }
